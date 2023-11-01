67aae6f6ee63cc1dc54fe492df9412fe78e6f22f
BriliantRustamova <briliantrustmova@gmail.com>
2023-11-02 02:19:06 +0400

    main.js-tasks

---
+++ 01.JS-variables.control-structure/main.js
@@ -0,0 +1,86 @@
+
+// I.Verilmiş 3 rəqəmli ədədin rəqəmləri cəmini,hasilini və ədədi ortasını tapan program tərtib edin. 
+// let number = 444;
+// let first = number % 10;
+
+// let second = ((number - first) / 10) % 10;
+// let third = ((number - first) / 10 - second) / 10;
+
+// console.log(`${first + second+ third}`);
+// console.log(`${first*second*third}`);
+// console.log(`${(first+second+third)/3}`)
+
+
+//II. Verilmis ededin butun bolenlerini tapan program tertib edin.
+// let a =50;
+// for (let index=0; index<=a; index ++){
+//     if (a%index ===0) console.log(index);
+// }
+
+//III. Verilmiş ədədin bölənlərinin sayını tapan proqram tərtib edin 
+// let a=100;
+// let count =0;
+// for (let index=0; index<=b; index ++){
+//     if ( b % index ===0)  {
+//     count++;
+// }
+//  }
+//  console.log(count);
+
+
+// IV.for loop'undan istifadə edərək aşağıdakı console'da aşağıdakı görüntünü alın. 
+
+// for (let a=0; a<=10; a++){
+//     console.log( `${a} *${a}=${a**2}`)
+// }
+
+// V.for loop'undan istifadə edərək aşağıdakı console'da aşağıdakı görüntünü alın.
+
+// console.log(`i  i*i  i*i*i`)
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i} ${i**2} ${i**3}`);
+// }
+
+
+
+
+
+// VI.0-100 arası yalniz sadə ededleri console'da çap edin.
+// let count=0
+// let i,j
+// for  (j=2 ; j<100 ;j++){
+//     for(i=1;i<=j;i++) {
+//          if(j%i==0 )
+//             count++
+//          }
+//          if(count==2)
+//          console.log(j)
+//         count=0
+// }
+
+// VI.0 - 100 arası tək ədədlərin və cüt ədədlərin cemini tapin 
+// let a=0;
+// let b=0;
+// for ( let i = 0; i <= 100; i++) {
+//     if ((i % 2) === 0) a += i; 
+// }
+// console.log(a)
+
+// for ( let i = 0; i <= 100; i++) {
+//   if ((i % 2) === 1) b += i; 
+// }
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
