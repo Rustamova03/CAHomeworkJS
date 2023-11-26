7e445880667af5d37b67fb2cdeca96d241192c11
BriliantRustamova <briliantrustmova@gmail.com>
2023-11-26 21:20:39 +0400

    changed

--- responsiv-novbar/app.js
+++ responsiv-novbar/app.js
@@ -1,14 +1,10 @@
-let toogleButton=document.querySelector(".toogle-button")
-let nav=document.querySelector("nav")
+let toogleButton = document.querySelector(".toogle-button");
+let nav = document.querySelector("nav");
 
-
-
-toogleButton.addEventListener('click',function(){
-nav.classList.toggle('active')
-})
-
-
+toogleButton.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
 let header = document.querySelector("header");
 window.addEventListener("scroll", function () {
-    header.classList.toggle("header-scroll", window.scrollY > 0);
-  });
+  header.classList.toggle("header-scroll", window.scrollY > 0);
+});
