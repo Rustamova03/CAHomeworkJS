6e8e8ca2ec8a00c7fb9984d98c1311f5b6edc4b8
BriliantRustamova <briliantrustmova@gmail.com>
2023-11-26 18:52:06 +0400

    changed

---
+++ responsiv-novbar/app.js
@@ -0,0 +1,14 @@
+let toogleButton=document.querySelector(".toogle-button")
+let nav=document.querySelector("nav")
+
+
+
+toogleButton.addEventListener('click',function(){
+nav.classList.toggle('active')
+})
+
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("header-scroll", window.scrollY > 0);
+  });
